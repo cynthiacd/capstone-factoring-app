391a45ca350deb4ac1f3b76a4b74642b3e3b68e2
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-07-08 11:01:32 -0700

    problem component is showing all information given in api response

--- src/components/trinomial_show.js
+++ src/components/trinomial_show.js
@@ -8,15 +8,10 @@ class TrinomialShow extends Component {
   }
 
   render() {
-    console.log(this.props.trinomial);
-    const trinomial = this.props.trinomial.trinomial;
-    // console.log(trinomial.trinomial);
-    // const promise = this.props.trinomial.trinomial;
-    // const { trinomial } = this.props;
-    // const data = trinomial.data;
-    // console.log(data);
-    // const trinomial = this.props.trinomial;
-    // console.log(trinomial);
+    // console.log(this.props.data);
+    const trinomial = this.props.data.trinomial;
+
+    // just like the course said - this conditional is important ...
     if (!trinomial) {
       return (
         <div>
@@ -29,14 +24,19 @@ class TrinomialShow extends Component {
       <div>
         <h3>problem goes here</h3>
         <p>You need to know what pattern you want to render the correct problem</p>
-        <p>{`x^2 + ${trinomial.b}x + ${trinomial.c}`}</p>
+        <p> Now getting pattern and string with api call</p>
+        <p>Pattern: { trinomial.pattern }</p>
+        <p>{ trinomial.general_form }</p>
+        <p>a: { trinomial.a }</p>
+        <p>b: { trinomial.b }</p>
+        <p>c: { trinomial.c }</p>
       </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  return { trinomial: state.trinomial };
+  return { data: state.trinomial };
 }
 
 export default connect(mapStateToProps, { fetchTrinomial })(TrinomialShow);
