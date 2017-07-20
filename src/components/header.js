f6606768868231491c9396cd13bda086f423c004
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-07-20 12:06:19 -0700

    rough draft of instructional info

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,29 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-class Header extends Component {
-
-  render() {
-    return (
-      <div className="header">
-        <Link to="/"><h1>Trinomial Factoring Practice</h1></Link>
-          <div className="navigation-bar">
-            <Link className="btn btn-primary" to="/learn_patterns">
-              Learn the Patterns
-            </Link>
-            <Link className="btn btn-primary" to="/learn_video">
-              Learn how to Factor
-            </Link>
-            <Link className="btn btn-primary" to="/practice/custom">
-              Practice
-            </Link>
-            <Link className="btn btn-primary" to="/user/new">
-              Sing Up
-            </Link>
-            <Link className="btn btn-primary" to="/user/login">
-              Log In
-            </Link>
-          </div>
-      </div>
-    );
-  }
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/"><h1>Trinomial Factoring Practice</h1></Link>
+        <div className="navigation-bar">
+          <Link className="btn btn-primary" to="/learn_patterns">
+            Learn the Patterns
+          </Link>
+          <Link className="btn btn-primary" to="/learn_video">
+            Learn how to Factor
+          </Link>
+          <Link className="btn btn-primary" to="/practice/custom">
+            Practice
+          </Link>
+          <Link className="btn btn-primary" to="/user/new">
+            Sing Up
+          </Link>
+          <Link className="btn btn-primary" to="/user/login">
+            Log In
+          </Link>
+        </div>
+    </div>
+  );
 }
+
 export default Header;
--- src/components/learn_patterns.js
+++ src/components/learn_patterns.js
@@ -6,9 +6,31 @@ const LearnPatterns = () => {
   // console.log("in render of video component");
   return (
     <div>
-      <Header />
+      <div><Header /></div>
+      <div className="instruction-info-patterns">
+        <h2></h2>
+          <p>Polynomial factoring is a critical skill of algebra. This app will
+          teach you how to factor quadratics/trinomials by identifiying the pattern
+          first.
+          </p>
+          <p>The patterns are based off of the signs and coefficients of the quadratic expression
+          in general form (ax<sup>2</sup> + bx + c)
+          </p>
+          <ul>
+            <li>Step 1: Look for a GCF</li>
+            <li>Step 2: Identifiy the Pattern</li>
+            <li>Step 3: Idenitfy the coefficients a, b, and c </li>
+            <li>Step 4: is a = 1 or a > 1?</li>
+            <li>Step 5: If a = 1, look for factors of c that add up to b and
+            dont forget the pattern - the signs of the factors must coresspond with the pattern</li>
+          </ul>
+      </div>
     </div>
   );
 }
 
 export default LearnPatterns;
+
+// could import a picture of factoring process ...
+// could have your flow chart and example
+// and box
