3ff22e10552ac5395735bf1c3ef023c9e2e6fe04
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-07-22 16:13:40 -0700

    authentication required requests are working

--- src/index.js
+++ src/index.js
@@ -6,36 +6,32 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
-// import { AUTH_USER } from './actions';
-
+import { AUTH_USER } from './actions/types';
 import App from './components/app';
 import RequireAuth from './components/auth/require_auth';
 import Signin from './components/auth/signin';
 // import Signup from './components/auth/singup';
 import Signout from './components/auth/signout';
-
+import Video from './components/instructional_video';
+import LearnPatterns from './components/learn_patterns';
 import TrinomialShow from './components/trinomial/trinomial_show';
 import Report from './components/trinomial/trinomial_progress_report';
 
-import Video from './components/instructional_video';
-import LearnPatterns from './components/learn_patterns';
 
 
 // promise is a middleware that we import - can write own if you want...
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
 
-// preparing code for token storage
-// const token = localStorage.getItem('token');
-// // If we have a token, consider the user to be signed in
-// if (token) {
-//   // we need to update application state
-//   store.dispatch({ type: AUTH_USER });
-// }
-
-
+// preparing token - if there is a token in the browser then user is signed in
+const token = localStorage.getItem('token');
+if (token) {
+  // we need to update application state
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={ createStoreWithMiddleware(reducers) }>
+  <Provider store={ store }>
     <Router history={browserHistory}>
       <Route path="/" component={ App }>
         <IndexRoute component={ LearnPatterns } />
