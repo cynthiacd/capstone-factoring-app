be3e38cffdf577e159ea25b6d2787abef0d789c5
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-07-24 12:27:47 -0700

    have fall back routes working - can hardcode in routes for localhost again

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,10 @@ module.exports = {
       }
     ]
   },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
