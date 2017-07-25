b83ff88d046620fa8458c2dbb4a786d0f20d4c37
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-07-25 09:17:30 -0700

    have production version created in dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify("production")
     }),
     new webpack.optimize.CommonsChunkPlugin({
       names: ['vendor', 'manifest']
