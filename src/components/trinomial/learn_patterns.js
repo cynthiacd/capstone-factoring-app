f8ad5bf968131afddaefb5fa7a31776adac3d508
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-07-26 20:40:12 -0700

    playing with styling of pattern info

--- src/components/trinomial/learn_patterns.js
+++ src/components/trinomial/learn_patterns.js
@@ -8,50 +8,40 @@ const LearnPatterns = () => {
       <div className="col-lg-3">
         <div className="pattern-card">
           <h4>Plus Plus</h4>
-          <h5>General Form:</h5>
-          <h5>x<sup>2</sup> + bx + c</h5>
-          <h5>Factored Form:</h5>
-          <h5>=(x+e)(x+f)</h5>
+          <h5>General Form: x<sup>2</sup> + bx + c</h5>
+          <h5>Factored Form: = (x+e)(x+f)</h5>
         </div>
       </div>
 
       <div className="col-lg-3">
         <div className="pattern-card">
           <h4>Minus Plus</h4>
-          <h5>General Form:</h5>
-          <h5>x<sup>2</sup> - bx + c</h5>
-          <h5>Factored Form:</h5>
-          <h5>=(x-e)(x-f)</h5>
+          <h5>General Form: x<sup>2</sup> - bx + c</h5>
+          <h5>Factored Form: = (x-e)(x-f)</h5>
         </div>
       </div>
 
       <div className="col-lg-3">
         <div className="pattern-card">
           <h4>Minus Minus</h4>
-          <h5>General Form:</h5>
-          <h5>x<sup>2</sup> - bx - c</h5>
-          <h5>Factored Form:</h5>
-          <h5>=(x+e)(x-f) where f > e</h5>
+          <h5>General Form: x<sup>2</sup> - bx - c</h5>
+          <h5>Factored Form: = (x+e)(x-f)</h5>
         </div>
       </div>
 
       <div className="col-lg-3">
         <div className="pattern-card">
           <h4>Plus Minus</h4>
-          <h5>General Form:</h5>
-          <h5>x<sup>2</sup> + bx - c</h5>
-          <h5>Factored Form:</h5>
-          <h5>=(x+e)(x-f) where e > f</h5>
+          <h5>General Form: x<sup>2</sup> + bx - c</h5>
+          <h5>Factored Form: =(x+e)(x-f)</h5>
         </div>
       </div>
 
       <div className="col-lg-3">
         <div className="pattern-card">
           <h4>Plus Double Square</h4>
-          <h5>General Form:</h5>
-          <h5>x<sup>2</sup> + 2ax + a<sup>2</sup></h5>
-          <h5>Factored Form:</h5>
-          <h5>=(x+a)(x+a) = (x+a)<sup>2</sup></h5>
+          <h5>General Form: x<sup>2</sup> + 2ax + a<sup>2</sup></h5>
+          <h5>Factored Form:=(x+a)(x+a)=(x+a)<sup>2</sup></h5>
         </div>
       </div>
 
@@ -59,27 +49,23 @@ const LearnPatterns = () => {
         <div className="pattern-card">
           <h4>Minus Double Square</h4>
           <h5>General Form: x<sup>2</sup> - 2ax - a<sup>2</sup></h5>
-          <h5>Factored Form: (x-a)(x-a) = (x-a)<sup>2</sup></h5>
+          <h5>Factored Form:=(x-a)(x-a) = (x-a)<sup>2</sup></h5>
         </div>
       </div>
 
       <div className="col-lg-3">
         <div className="pattern-card">
           <h4>Difference of Squares</h4>
-          <h5>General Form:</h5>
-          <h5>x<sup>2</sup> - a<sup>2</sup></h5>
-          <h5>Factored Form:</h5>
-          <h5>=(x+a)(x-a)</h5>
+          <h5>General Form: x<sup>2</sup> - a<sup>2</sup></h5>
+          <h5>Factored Form: =(x+a)(x-a)</h5>
         </div>
       </div>
 
       <div className="col-lg-3">
         <div className="pattern-card">
           <h4>a is greater than one (a > 1)</h4>
-          <h5>General Form:</h5>
-          <h5>ax<sup>2</sup> + bx + c</h5>
-          <h5>Factored Form:</h5>
-          <h5>=(ax+e)(x+f)</h5>
+          <h5>General Form: ax<sup>2</sup> + bx + c</h5>
+          <h5>Factored Form: =(ax+e)(x+f)</h5>
         </div>
       </div>
     </div>
