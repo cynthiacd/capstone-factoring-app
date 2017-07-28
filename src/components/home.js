1eb2af925337fc53f48087f1c78fe12589625a1e
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-07-27 20:23:56 -0700

    added text to welcome component

--- src/components/home.js
+++ src/components/home.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Home = () => {
   return (
     <div className="instruction-info-patterns">
       <h1>Welcome</h1>
-        <p>Polynomial factoring is a critical skill of algebra. This app will
-        teach you how to factor quadratics/trinomials by identifiying the pattern
-        first.
+
+        <p>You have found yourself at an <a href="https://adadevelopersacademy.org/" target="_blank">ADA </a>
+         developer’s capstone project.</p>
+
+        <p>This site was a self-directed project that I took from concept to production.
+        I built this app with React and Redux on the frontend and a Rails API for the backend.
+        This app was inspired by my previous career as a math and
+        science teacher/tutor. 5 years of teaching students one-on-one, taught me
+        that many students struggle with specific patterns within one type of problem.
         </p>
-        <p>The patterns are based off of the signs and coefficients of the quadratic expression
-        in general form (ax<sup>2</sup> + bx + c)
+
+        <p>For example quadratic factoring. While the overall strategies to factoring
+        are the same for all problems, it is common for students to struggle with
+        the signs. The signs of the expression can be catorigized by patterns
+        and identifiying these patterns can help you solve the problem.
+        As a tutor, I observed this over and over again and found providing
+        students with extra practice on the specific patterns they struggled with lead to the biggest improvements.
         </p>
-        <ul>
-          <li>Step 1: Look for a GCF</li>
-          <li>Step 2: Identifiy the Pattern</li>
-          <li>Step 3: Idenitfy the coefficients a, b, and c </li>
-          <li>Step 4: is a = 1 or a > 1?</li>
-          <li>Step 5: If a = 1, look for factors of c that add up to b and
-          dont forget the pattern - the signs of the factors must coresspond with the pattern</li>
-        </ul>
+
+        <p>But I always wanted a way to serve unlimited targeted practice ... </p>
+
+        <p>Click on <Link to="/signup">sign up</Link> to start to see more and start factoring</p>
     </div>
+
+
   );
 }
 
